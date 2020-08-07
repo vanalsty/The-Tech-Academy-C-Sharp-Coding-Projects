9192e686d7476aff11f5e01782ee39a40724c3cf
Laurie Van Alstyne <laurie.vanalstyne@gmail.com>
2020-08-07 08:35:37 -0700

    Update auto increment

--- CarInsurance/CarInsurance/Models/InsuranceEntities.Designer.cs
+++ CarInsurance/CarInsurance/Models/InsuranceEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\lauri\Desktop\C#Projects\CarInsurance\CarInsurance\Models\InsuranceEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\lauri\Desktop\The-Tech-Academy-C-Sharp-Coding-Projects\CarInsurance\CarInsurance\Models\InsuranceEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
